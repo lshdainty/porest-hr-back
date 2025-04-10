269c522cd53d9fed361520f8719d4cb9e7802323
lshdainty <lshdainty@gmail.com>
2025-04-10 17:12:14 +0900

    spring security 임시 주석 - react와 통신 테스트를 위한 임시 비활성화

--- src/main/java/com/lshdainty/myhr/lib/jwt/JwtUtil.java
+++ src/main/java/com/lshdainty/myhr/lib/jwt/JwtUtil.java
@@ -1,43 +1,43 @@
-package com.lshdainty.myhr.lib.jwt;
-
-import io.jsonwebtoken.Jwts;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-
-@Slf4j
-@Component
-public class JwtUtil {
-    private SecretKey secretKey;
-
-    public JwtUtil(@Value("${jwt.secret}") String secret) {
-        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
-    }
-
-    public Long getUserName(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("userNo", Long.class);
-    }
-
-    public String getRole(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
-    }
-
-    public Boolean isExpired(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
-    }
-
-    public String generateToken(Long userNo, String role, Long expiredMs) {
-        return Jwts.builder()
-                .claim("userNo", userNo)
-                .claim("role", role)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()+ expiredMs))
-                .signWith(secretKey)
-                .compact();
-    }
-}
+//package com.lshdainty.myhr.lib.jwt;
+//
+//import io.jsonwebtoken.Jwts;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import javax.crypto.SecretKey;
+//import javax.crypto.spec.SecretKeySpec;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Date;
+//
+//@Slf4j
+//@Component
+//public class JwtUtil {
+//    private SecretKey secretKey;
+//
+//    public JwtUtil(@Value("${jwt.secret}") String secret) {
+//        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+//    }
+//
+//    public Long getUserName(String token) {
+//        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("userNo", Long.class);
+//    }
+//
+//    public String getRole(String token) {
+//        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+//    }
+//
+//    public Boolean isExpired(String token) {
+//        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+//    }
+//
+//    public String generateToken(Long userNo, String role, Long expiredMs) {
+//        return Jwts.builder()
+//                .claim("userNo", userNo)
+//                .claim("role", role)
+//                .issuedAt(new Date(System.currentTimeMillis()))
+//                .expiration(new Date(System.currentTimeMillis()+ expiredMs))
+//                .signWith(secretKey)
+//                .compact();
+//    }
+//}
